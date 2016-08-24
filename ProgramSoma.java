6db0fc2cb38ba0b3bd2d9e6fe1be92a822df157f
Max <maxgomesrj@gmail.com>
2016-08-23 21:27:15 -0300

    Add files via upload

---
+++ ProgramSoma.java
@@ -0,0 +1,12 @@
+
+public class ProgramSoma {
+	static void soma(int a, int b){
+		int resultado = a+b;
+		System.out.println("O resultado da soma é: "+resultado);
+	}
+	
+	public static void main(String [] args){
+		soma(5,3);
+	}
+	
+}
